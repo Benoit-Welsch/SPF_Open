2d107038c8ae5ad9da68232f2c0d9a6c6a6a42af
Benoit Welsch <benoitwelsch@protonmail.com>
2022-12-02 11:26:03 +0100

    Feat : copyright

--- tao_export/src/App.svelte
+++ tao_export/src/App.svelte
@@ -2,7 +2,14 @@
   import ZipInput from "./lib/ZipInput.svelte";
 </script>
 
+<header>&copy;Benoit-Welsch - Dec-2022</header>
 <main><ZipInput /></main>
 
 <style>
+  header {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 </style>
