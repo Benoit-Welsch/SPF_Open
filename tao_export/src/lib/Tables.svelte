994b419c807886e032500984d281730b43a202d4
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-11-09 08:27:27 +0000

    Feat : Select question by text

--- tao_export/src/lib/Tables.svelte
+++ tao_export/src/lib/Tables.svelte
@@ -4,6 +4,16 @@
   export let questions: QuestionType[] = [];
   let checked = false;
 
+  let text = "";
+
+  $: if (text) {
+    const qn = text.split(",").map((n) => n.trim());
+    questions = questions.map((q, n) => ({
+      ...q,
+      show: qn.includes(q.title.split(" ")[1]),
+    }));
+  }
+
   const changeSelection = () => {
     console.log("changeSelection", checked);
     questions = questions.map((q) => ({ ...q, show: checked }));
@@ -13,6 +23,10 @@
 {#if !questions || questions.length}
   <div class="hide-print">
     <h3>Show/Hide Question</h3>
+    <div>
+      <input type="text" id="SelectQuestion" bind:value={text} />
+      <label for="SelectQuestion">ex : 12,13,15</label>
+    </div>
     <ul>
       <li>
         <input type="checkbox" bind:checked on:change={changeSelection} />
@@ -29,12 +43,16 @@
 {/if}
 
 <style>
-  div{
+  label{
+    margin-left: 4px;
+    filter: opacity(0.5);
+  }
+  div {
     overflow-y: scroll;
     flex: 1;
     max-height: 100%;
   }
-  h3{
+  h3 {
     margin: 0;
   }
   ul {
